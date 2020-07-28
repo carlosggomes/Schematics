2bd5608972890287e94b8445205de6cf4478a54a
Guarany <33207513+carlosggomes@users.noreply.github.com>
2020-07-28 15:09:12 -0300

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -59,7 +59,7 @@ resource ibm_is_instance "vsi1" {
   zone    = "${local.ZONE}"
   keys    = ["${data.ibm_is_ssh_key.ssh_key_id.id}"]
   image   = "${data.ibm_is_image.ubuntu.id}"
-  profile = "cc1-2x4"
+  profile = "bx2-2x8"
 
   primary_network_interface = {
     subnet          = "${ibm_is_subnet.subnet1.id}"
